ca4ffb0829e2344a965843391670242a62537a20
AbiyaLS <abiyals9876@gmail.com>
2026-02-05 12:11:09 +0530

    admin authenication completed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { createProduct, getSingleProduct, listProducts, removeProduct } from "../controller/productController.js"
 import upload from "../middleware/multer.js"
+import adminAuth from "../middleware/auth/adminAuth.js"
 
 const router = express.Router()
 // this middleware is use to upload image of producr in file as a form data
-router.post("/add", 
+router.post("/add", adminAuth,
     upload.fields([
         { name: "image1", maxCount: 1 },
         { name: "image2", maxCount: 1 },
@@ -14,7 +15,7 @@ router.post("/add",
     createProduct)
 
 router.get("/list",listProducts)
-router.delete("/remove",removeProduct)
+router.delete("/remove",adminAuth,removeProduct)
 router.post("/single",getSingleProduct)
 
 export default router
